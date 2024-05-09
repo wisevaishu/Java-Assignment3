7e963bd51f5359b97c82cc16ae30087d71d84ca0
wisevaishu <vpm.win@gmail.com>
2024-05-09 16:05:02 -0500

    Java GA 3 Completed with Actions

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -48,6 +48,7 @@ public class ListController {
     }
 
     @GetMapping(value = "jobs")
+
     public String listJobsByColumnAndValue(Model model, @RequestParam String column,
                                            @RequestParam(required = false) String value) {
         ArrayList<Job> jobs;
